cb097f08042e0b28b2e54d58175223127649a19f
Marco Whyte <mwhyte@umass.edu>
2020-02-04 11:10:45 -0600

    working forms for login and registration

--- spotify/src/components/Register.js
+++ spotify/src/components/Register.js
@@ -1,33 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
 import styled from "styled-components";
-import '../App.css';
+import "../App.css";
 import axios from "axios";
-import { Link } from 'react-router-dom';
-
+import { Link } from "react-router-dom";
+import { Spinner } from "react-spinkit";
 
 const WholeForm = styled.div`
-    width: 100%;  
-    height: 100vh;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    align-items: center;
-    background: linear-gradient(purple, red);
-`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  background: linear-gradient(purple, red);
+`;
 const Display = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-between;
-    border: 1px solid black;
-    width: 500px;
-    background: #fff;
-    border-radius: 10px;
-    overflow: hidden;
-    padding: 42px 55px 45px 55px;
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  border: 1px solid black;
+  width: 500px;
+  background: #fff;
+  border-radius: 10px;
+  overflow: hidden;
+  padding: 42px 55px 45px 55px;
+`;
 const Label = styled.label`
   display: flex;
   flex-direction: column;
@@ -37,131 +37,160 @@ const Label = styled.label`
   margin-top: 5%;
 `;
 const Button = styled.button`
-    width: 200px;
-    background: linear-gradient(purple, red);
-    font-weight: 600;
-    color: white;
-    cursor: pointer;
-    margin: 20px;
-    height: 50px;
-    text-align:center;
-    border: none;
-    background-size: 300% 100%;
-    border-radius: 50px;
-`
+  width: 200px;
+  background: linear-gradient(purple, red);
+  font-weight: 600;
+  color: white;
+  cursor: pointer;
+  margin: 20px;
+  height: 50px;
+  text-align: center;
+  border: none;
+  background-size: 300% 100%;
+  border-radius: 50px;
+`;
 
-const Register = ({ values, errors, touched, status}) => {
-    
-    // useEffect(() => {
-    //     console.log("status change", status)
-    //     status && setUsers( users =>
-    //         [...users, status])
-
-    // }, [status]);
-    
-
-return (
+const Register = ({ values, errors, touched, status }) => {
+  return (
     <WholeForm>
-        <Display>
+      <Display>
         <Form>
-        <h2>Register</h2>
-        {/* <Label htmlFor="firstname">
-                    <Field className= "border" id="firstname" type="text" name="firstname" placeholder="first name"/>
-                    {touched.firstname && errors.firstname && (
-                        <p className="hasError">{errors.firstname}</p>
-                    )}
-        </Label>
-        <Label htmlFor="lastname">
-                    <Field className= "border" id="lastname" type="text" name="lastname" placeholder="last name"/>
-                    {touched.lastname && errors.lastname && (
-                        <p className="hasError">{errors.lastname}</p>
-                    )}
-        </Label> */}
-        <Label htmlFor="username">
-                    <Field className="border" id="username" type="text" name="username" placeholder="username"/>
-                    {touched.username && errors.username && (
-                        <p className="hasError">{errors.username}</p>
-                    )}
-        </Label>
-        {/* <Label htmlFor="email">
-                    <Field className="border" id="email" type="text" name="email" placeholder="email"/>
-                    {touched.email && errors.email && (
-                        <p className="hasError">{errors.email}</p>
-                    )}
-        </Label> */}
-        <Label htmlFor="password">
-                    <Field className="border" id="password" type="text" name="password" placeholder="password"/>
-                    {touched.password && errors.password && (
-                        <p className="hasError">{errors.password}</p>
-                    )}
-        </Label>
-        {/* <Label htmlFor="password">
-                    <Field className="border" id="passwordconfirm" type="text" name="passwordconfirm" placeholder="password confirm"/>
-                    {touched.passwordconfirm && errors.passwordconfirm && (
-                        <p className="hasError">{errors.passwordconfirm}</p>
-                    )}
-        </Label> */}
-        <Button type="submit">Sign Up</Button>
+          <h2>Register</h2>
+          <Label htmlFor="firstName">
+            <Field
+              className="border"
+              id="firstName"
+              type="text"
+              name="firstName"
+              placeholder="first name"
+            />
+            {touched.firstName && errors.firstName && (
+              <p className="hasError">{errors.firstName}</p>
+            )}
+          </Label>
+          <Label htmlFor="lastName">
+            <Field
+              className="border"
+              id="lastName"
+              type="text"
+              name="lastName"
+              placeholder="last name"
+            />
+            {touched.lastName && errors.lastName && (
+              <p className="hasError">{errors.lastName}</p>
+            )}
+          </Label>
+          <Label htmlFor="username">
+            <Field
+              className="border"
+              id="username"
+              type="text"
+              name="username"
+              placeholder="username"
+            />
+            {touched.username && errors.username && (
+              <p className="hasError">{errors.username}</p>
+            )}
+          </Label>
+          <Label htmlFor="email">
+            <Field
+              className="border"
+              id="email"
+              type="text"
+              name="email"
+              placeholder="email"
+            />
+            {touched.email && errors.email && (
+              <p className="hasError">{errors.email}</p>
+            )}
+          </Label>
+          <Label htmlFor="password">
+            <Field
+              className="border"
+              id="password"
+              type="text"
+              name="password"
+              placeholder="password"
+            />
+            {touched.password && errors.password && (
+              <p className="hasError">{errors.password}</p>
+            )}
+          </Label>
+          <Label htmlFor="passwordconfirm">
+            <Field
+              className="border"
+              id="passwordconfirm"
+              type="text"
+              name="passwordconfirm"
+              placeholder="password confirm"
+            />
+            {touched.passwordconfirm && errors.passwordconfirm && (
+              <p className="hasError">{errors.passwordconfirm}</p>
+            )}
+          </Label>
+          <Button type="submit">Sign Up</Button>
         </Form>
         <Link to="/">
-        <button className="button">Already have an account? Sign In</button>
+          <button className="button">Already have an account? Sign In</button>
         </Link>
-        </Display>
+      </Display>
     </WholeForm>
-    );
+  );
 };
 
 const ForMikRegister = withFormik({
-    mapPropsToValues({ firstname, lastname, username, email, password, passwordconfirm}){
-        return {
-            // firstname: firstname || "",
-            // lastname: lastname || "",
-            username: username || "",
-            // email: email || "",
-            password: password || "",
-            // passwordconfirm: passwordconfirm || ""
-        }
-    },
-    validationSchema: Yup.object().shape({
-        // firstname: Yup
-        // .string()
-        // .required("First Name is Required"),
-        // lastname: Yup
-        // .string()
-        // .required("Last Name is Required"),
-        username: Yup
-        .string()
-        .required("Username is Required"),
-        // email: Yup
-        // .string()
-        // .email()
-        // .required("Email is Required"),
-        password: Yup
-        .string()
-        .min(8)
-        .max(16)
-        .required("Password is Required"),
-        // passwordconfirm: Yup
-        // .string()
-        // .oneOf([Yup.ref('password'), null])
-        // .required('Password must Match')
-    }),
-    handleSubmit(values, {setStatus, resetForm}) {
-        console.log("submitting", values);
-        axios.post("https://spotify-song-suggester-3.herokuapp.com/api/auth/signup", values)
-        .then(response => {
-            console.log(response.data)
-            console.log(response)
-        })
-        .catch(error => console.log(error.response))
-        // console.log("submitted First name:", values.firstname)
-        // console.log("submitted Last name:", values.lastname)
-
-        // console.log("submitted email:", values.email)
-        console.log("submitted username:", values.username)
-        console.log("submitted password:", values.password)
-        // console.log("password matches:", values.passwordconfirm)
-    }
+  mapPropsToValues({
+    firstName,
+    lastName,
+    username,
+    email,
+    password,
+    passwordconfirm
+  }) {
+    return {
+      firstName: firstName || "",
+      lastName: lastName || "",
+      username: username || "",
+      email: email || "",
+      password: password || "",
+      passwordconfirm: passwordconfirm || ""
+    };
+  },
+  validationSchema: Yup.object().shape({
+    firstName: Yup.string().required("First Name is Required"),
+    lastName: Yup.string().required("Last Name is Required"),
+    username: Yup.string().required("Username is Required"),
+    email: Yup.string()
+      .email()
+      .required("Email is Required"),
+    password: Yup.string()
+      .min(8)
+      .max(16)
+      .required("Password is Required"),
+    passwordconfirm: Yup.string().oneOf(
+      [Yup.ref("password"), null],
+      "Passwords must match"
+    )
+  }),
+  handleSubmit(values, { setStatus, resetForm }) {
+    console.log("submitting", values);
+    axios
+      .post(
+        "https://spotify-song-suggester-3.herokuapp.com/api/auth/signup",
+        values
+      )
+      .then(response => {
+        console.log(response.data);
+        console.log(response);
+      })
+      .catch(error => console.log(error.response));
+    console.log("submitted First name:", values.firstName);
+    console.log("submitted Last name:", values.lastName);
+    console.log("submitted email:", values.email);
+    console.log("submitted username:", values.username);
+    console.log("submitted password:", values.password);
+    console.log("password matches:", values.passwordconfirm);
+  }
 })(Register);
 
 export default ForMikRegister;
