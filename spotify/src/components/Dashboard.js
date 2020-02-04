62052e600a282c14019f294a35597a2eb975e58d
gavin stahl <stahlgavin@gmail.com>
2020-02-04 09:08:35 -0800

    small change

--- spotify/src/components/Dashboard.js
+++ spotify/src/components/Dashboard.js
@@ -4,7 +4,7 @@ import {Button} from 'reactstrap';
 function Dashboard() {
   return (
     <div>
-      <h1 className="App-header">Welcome To The User Dashboard</h1>
+      <h1 className="App-header">Welcome To Your Dashboard</h1>
       <div className="title-container">
         <h2>Let's Find Some New Music!</h2>
         <Button color="primary">Browse</Button>
@@ -37,7 +37,7 @@ function Dashboard() {
           </form>
           <Button className="profile-div" color="primary">Edit Profile</Button>
         </div>
-        <div className="split-container">
+        <div className="split-container fav-container">
         <h2>Favorite Songs List</h2>
           <div className="favorites-list">
             <p>song: test</p>
