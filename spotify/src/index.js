55bcd18cb93ca4bb6135b8655a7a89c31c2037f9
Marco Whyte <39717954+mwhyte18@users.noreply.github.com>
2020-02-06 17:47:31 -0600

    Update index.js

--- spotify/src/index.js
+++ spotify/src/index.js
@@ -4,11 +4,10 @@ import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import { createStore, applyMiddleware } from 'redux';
 import {Provider } from 'react-redux';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import {reducer} from './reducers/index';
 
 
